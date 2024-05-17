0ac1bb11ae94727ff0c539741632f68e4e7a8360
Nitish sharma <75838617+Nitz2611@users.noreply.github.com>
2024-05-17 16:18:46 +0530

    typeaias done

--- object.js
+++ object.js
@@ -1,6 +1,20 @@
 "use strict";
+// typealias this is typescript feature
+//if we want to create more than one object with same property we can define typealias at beginning
+//object
+// const person:{
+//     name:string;
+//     age:number;
+//     isStudent:boolean
+// } = {
+//     name : "Nitish",
+//     age: 25,
+//     isStudent: false
+// }
+// now we can write this as 
+// so we dont have define the type everytime
 const person = {
-    name: "Nitish",
+    name: 'Nitish',
     age: 25,
     isStudent: false
 };
--- object.ts
+++ object.ts
@@ -1,12 +1,30 @@
-const person:{
-    name:string;
-    age:number,
-    isStudent:boolean
-} = {
-    name : "Nitish",
+// typealias this is typescript feature
+//if we want to create more than one object with same property we can define typealias at beginning
+
+type Person = {
+    name: string;
+    age: number;
+    isStudent: boolean;
+}
+
+//object
+
+// const person:{
+//     name:string;
+//     age:number;
+//     isStudent:boolean
+// } = {
+//     name : "Nitish",
+//     age: 25,
+//     isStudent: false
+
+// }
+// now we can write this as 
+// so we dont have define the type everytime
+const person:Person = {
+    name: 'Nitish',
     age: 25,
     isStudent: false
-
 }
 
 console.log(person)
